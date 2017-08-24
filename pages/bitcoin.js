d3368cf3eb74e6705017c3e5aa0c1dd8ddd046ce
William T <william.tam18@gmail.com>
2017-08-24 15:16:54 +0800

    svg background

--- pages/bitcoin.js
+++ pages/bitcoin.js
@@ -1,4 +1,6 @@
-function Background(width, height) {
+import { withScreenSize } from '@vx/responsive'
+
+function Background({width, height}) {
     return (
         <svg width={width} height={height}>
             <rect 
@@ -19,13 +21,24 @@ class App extends React.Component {
     }
 
     render() {
+        const { screenWidth, screenHeight } = this.props
         const { data } = this.state
         return (
             <div>
-                <Background width={100} height={100} />
+                <Background width={screenWidth} height={screenHeight} />
+                <style jsx>{`
+                    .app {
+                        display: flex;
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0; 
+                    }
+                `}</style>
             </div>
         )
     }
 }
 
-export default App
+export default withScreenSize(App)
